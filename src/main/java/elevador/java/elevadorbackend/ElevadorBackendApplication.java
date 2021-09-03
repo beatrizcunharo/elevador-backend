f5c96fc268fee9bf6065045c8d9ffcb50753e0cd
beatrizcunharo <beatrizcunharo@gmail.com>
2021-09-03 11:30:48 -0300

    fix: Cors configuration

--- src/main/java/elevador/java/elevadorbackend/ElevadorBackendApplication.java
+++ src/main/java/elevador/java/elevadorbackend/ElevadorBackendApplication.java
@@ -2,11 +2,25 @@ package elevador.java.elevadorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ElevadorBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElevadorBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
